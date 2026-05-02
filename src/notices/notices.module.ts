51cb909a5d92c212d6c54f5f3869dfb7c819a57b
Rahul15205 <rahulkumarshukla4627@gmail.com>
2026-05-02 11:06:09 +0530

    fix

--- src/notices/notices.module.ts
+++ src/notices/notices.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { NoticesController } from './notices.controller';
 import { NoticePublicController } from './notice-public.controller';
 import { NoticesService } from './notices.service';
+import { TranslationModule } from '../translation/translation.module';
 
 @Module({
+  imports: [TranslationModule],
   controllers: [NoticesController, NoticePublicController],
   providers: [NoticesService],
   exports: [NoticesService],
